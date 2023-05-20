82b3680264419c3e7904d883c6f7ac4322640a72
monireh.rahmani <monireh.rahmani@email.part.ir>
2023-05-20 14:51:21 +0330

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MadDic_python
 Fun beginner Python projects
+The Mad Libs Generator gathers and manipulates user input data as an adjective, a pronoun, and a verb.
+The program takes this data and arranges it to build a story.
---
+++ madLibs.py
@@ -0,0 +1,21 @@
+
+#Mad Libs Generator
+
+
+# Question for the user to answer
+noun = input('choose a noun: ' )
+p_noun = input('Choose a plural noun: ')
+noun2 = input('Choose a noun: ')
+place = input('Name a place: ')
+adjective = input('Choose an adjective(Describing world): ')
+noun3 = input('Choose a noun: ')
+
+# Print a story from thr user
+print('------------------------------------------')
+print('Be kind to your', noun, '- footed', p_noun)
+print('For a duck may be somebody\'s', noun2, ',')
+print('Be kind to your', p_noun, 'in', place)
+print('Where the weather is always', adjective, '. \n')
+print('You may think that is this the', noun3, ',')
+print('Well it is.')
+print('------------------------------------------')
